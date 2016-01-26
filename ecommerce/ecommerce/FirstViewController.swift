394d82721f3483123a05c97aafba8bfc47c63dbc
stayzilla <asif.junaid@stayzilla.com>
2016-01-26 18:54:40 +0530

    initial commit

---
+++ ecommerce/ecommerce/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ecommerce
+//
+//  Created by Asif Junaid on 1/26/16.
+//  Copyright © 2016 Asif Junaid. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ecommerce/ecommerce/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ecommerce
+//
+//  Created by Asif Junaid on 1/26/16.
+//  Copyright © 2016 Asif Junaid. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
